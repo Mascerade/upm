6e1bf96cf73411c253fa5dc56e5896675c0840c0
BinaryWiz <Ultimi450@gmail.com>
2019-05-24 08:45:11 -0400

    documented code

--- Source/Product.cpp
+++ Source/Product.cpp
@@ -66,34 +66,49 @@ class Product {
 		vector<Token> Tokens;
 
 		Product(string vendor, string title) {
+			// Set the attributes of of the product
 			this->vendor = vendor;
 			this->title = title;
 		}
 
 		void generate_tokens() {
+			// Appends the tokens tokenized_title vector
+
+			// Check1 is used to parse the string by space 
 			stringstream check1(this->title);
 			string temp;
+
+			// Goes through each token and gets rid of the punctuation from the token
 			while (getline(check1, temp, ' ')) {
 				remove_if(temp.begin(), temp.end(), ::isspace);
 				for(char character : punctuations) {
 					temp.erase(remove(temp.begin(), temp.end(), character), temp.end());
 				}
 
+				// Converts the token to lower case letters
 				transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
 
+				// Makes sure no duplicates are added to the title
 				if (tokenized_title.size() != 0) {
+					
+					// If add is true, add the token to the tokenized_title
 					bool add = true;
 					for (string x : tokenized_title) {
+						
+						// If the same token is found, set add to false and don't add it to the tokenized_title
 						if(temp == x) {
 							add = false;
 							break;
 						}
 					}
+
+					// Add to the tokenized_title if add is true
 					if (add)
 						tokenized_title.push_back(temp);
 				}
 
 				else {
+					// For the first word in title add it to the tokenized_title
 					tokenized_title.push_back(temp);
 				}
 
@@ -101,6 +116,7 @@ class Product {
 		}
 
 		void token_concatenator() {
+			// Makes sure that attributes that are "alone" are appended to the previous value
 			for(int i = 0; i < tokenized_title.size(); i++) {
 				for(string attribute : attributes) {
 					if(tokenized_title[i] == attribute) {
@@ -116,7 +132,8 @@ class Product {
 				// Must make sure there are no duplicates (use the hash id)
 		}
 
-		void execute() { 
+		void execute() {
+			// Runs the necessary functions to generate tokens and correctly format them according to UPM 
 			generate_tokens();
 			token_concatenator();
 			for (string x : tokenized_title) {
