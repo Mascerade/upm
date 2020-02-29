328041d57707e1bb3f40a1858a0ebad12d594f58
BinaryWiz <Ultimi450@gmail.com>
2020-02-29 16:13:24 -0500

    add build.sh and changed Product.cpp to product.cpp

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd /home/jason/Documents/UPM/build
+g++ -std=c++11 -c ../src/token.cpp ../src/combination.cpp ../src/common.cpp ../src/product.cpp ../src/main.cpp -I ../include/
+g++ --std=c++11 -o ../bin/main.exe ./token.o ./combination.o ./common.o ./product.o ./main.o
+echo Finished compiling and creating executable with no error
